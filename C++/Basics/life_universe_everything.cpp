73ad49acec3ae78e1d60af928fabf938047d559f
hartejnayar <hartejnayar11@gmail.com>
2026-09-08 23:32:47 +0530

    add basics solutions

---
+++ C++/Basics/life_universe_everything.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long;
+
+int main() {
+	while(true){
+		int n;
+		cin >> n;
+		if(n == 42){
+			break;
+		}
+		cout << n << endl;
+	}
+    return 0;
+}
