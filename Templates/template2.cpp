77792fd277f1f672d82188397bc358c0e5f33cb8
Hartej Nayar <71260380+hartejnayar@users.noreply.github.com>
2026-09-23 21:21:47 +0530

    Create template2.cpp

---
+++ Templates/template2.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    // Write your code for each test case here
+}
+
+int main() {
+    // Fast I/O
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin >> t; // Remove if there is only one test case
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
